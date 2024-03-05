69b95fe159508a749998d80e7a102174beb5406b
ryanef <ryanef39@gmail.com>
2024-03-05 15:05:13 -0500

    Be (#3)
    * data outputs
    
    * ecs loadbalancer

--- locals.tf
+++ locals.tf
@@ -5,8 +5,6 @@ locals {
       environment = "dev"
       target_type = "ip"
       vpc_name = "TFE_CONTROLLER"
-      vpc_id = data.terraform_remote_state.vpc.outputs.vpc_id
-      subnets = data.terraform_remote_state.vpc.outputs.public_subnets
-    }
   }
+}
 }
